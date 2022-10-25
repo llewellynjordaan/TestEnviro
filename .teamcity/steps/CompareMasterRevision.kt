284fc647f91aed5421ba4ab957168f41c715fe11
XEROINSIDE\reece.gibbs <reece.gibbs@xero.com>
2022-10-25 19:56:39 +1300

    Output

--- .teamcity/steps/CompareMasterRevision.kt
+++ .teamcity/steps/CompareMasterRevision.kt
@@ -10,6 +10,9 @@ class CompareMasterRevision: PowerShellStep ({
             ${'$'}masterRevision = git rev-parse refs/heads/master | Out-String
             ${'$'}buildRevision = "%build.vcs.number%"
             
+            Write-Output "Master Revision: " + ${'$'}masterRevision
+            Write-Output "Build Revision: " + ${'$'}buildRevision
+            
             if (${'$'}buildBranch.Trim() -ne "refs/heads/master" -and ${'$'}masterRevision.Trim() -eq ${'$'}buildRevision.Trim()) {
                 write-output "condition matched to cancel the build"
                 write-host "##teamcity[buildStop comment='Branch Up To Date With Master - Please Run Master Build' readdToQueue='false']"
